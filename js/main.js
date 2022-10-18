b25ab506d2337f70a3eccbe86fe8c94f942522f6
Paul5070 <108010905+Paul5070@users.noreply.github.com>
2022-10-18 16:53:32 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  alert("Welcome to Paul's Fan Page");
+
+  $('#go-button').on('click', function() {
+    alert("Feel the power of the Dark Side!");
+  });
+
+
+
+
+
+
+
+});
